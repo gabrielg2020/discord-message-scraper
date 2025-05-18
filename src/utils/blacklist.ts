f5992c6f5ea4072a4e86456cdb51315898d47e79
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-05-18 15:10:13 +0100

    feat: ignore messages from blacklisted members
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ src/utils/blacklist.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class Blacklist {
+  private readonly members: string[];
+
+  constructor() {
+    try {
+      const filepath = path.join(__dirname, '../blacklist.json')
+      const fileData = fs.readFileSync(filepath, 'utf-8');
+      this.members = JSON.parse(fileData);
+    } catch (error) {
+      this.members = [];
+      console.error(`An error occured when creating blacklist: `, error);
+      throw error;
+    }
+  }
+
+  public get memebers(): string[] {
+    return this.members;
+  }
+
+  public isMemberBlacklisted(username: string): boolean {
+    return this.members.includes(username)
+  }
+}
